ebc60c1641d89d0646374cb7faefd0d3aaa69dcd
Aidy199258 <444283027@qq.com>
2020-11-07 17:43:28 +1300

    Removed unnecessary codes

--- src/com/ECS/System.java
+++ src/com/ECS/System.java
@@ -23,65 +23,6 @@ public abstract class System {
 
 }
 
-/*class User_Input extends System implements KeyListener {
-
-public  static boolean right = false;
-public  static boolean left = false;
-
-public static boolean jumping = false;
-public static long jumpingTime = 200;
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        e.getKeyCode() == KeyEvent.VK_RIGHT){
-            right = true;
-            //mX++;//this suppose to go right when right key press however no drawing att the moment so i just comment out
-        }
-        e.getKeyCode() == KeyEvent.VK_LEFT){
-            left = true;
-            //mX--;//this suppose to go left when right key press however no drawing att the moment so i just comment out
-        }
-        e.getKeyCode() == KeyEvent.VK_SPACE){
-            jumping = true;
-            new Thread(new thread()).start();
-
-        }
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        e.getKeyCode() == KeyEvent.VK_RIGHT){
-            right = false;
-
-        }
-        e.getKeyCode() == KeyEvent.VK_LEFT){
-            left = false;
-        }
-        e.getKeyCode() == KeyEvent.VK_SPACE){
-            jumping = false;
-        }
-    }
-
-    public class thread implements Runnable{
-
-        @Override
-        public void run() {
-            try{
-                Thread.sleep(jumpingTime);
-                jumping = false;
-            } catch (Exception e){
-                e.printStackTrace();
-                new Thread(this).start();
-                System.exit(0);
-            }
-        }
-    }*/
-
 
 //System to load image
 class LoadImageSystem extends System{
@@ -165,47 +106,7 @@ class LoadImageSystem extends System{
 
 }
 
-class DrawImageSystem extends System{
-     static Graphics2D mGraphics;
 
-    //Load images at initialisation of Game Panel
-    public DrawImageSystem(ArrayList<Entity> entities){
-        //Draw Character
-
-
-        //Draw Platforms
-
-        //Draw Coins
-    }
-
-    // Draws an image on the screen at position (x,y)
-    public DrawImageSystem(Image image, double x, double y){
-        // Check if image is null
-        if(image == null) {
-            // Print Error message
-            java.lang.System.out.println("Error: cannot draw null image.\n");
-            return;
-        }
-
-        // Draw image on screen at (x,y)
-        mGraphics.drawImage(image, (int)x, (int)y, null);
-
-    }
-
-    // Draws an image on the screen at position (x,y)
-    public  DrawImageSystem(Image image, double x, double y, double w, double h) {
-        // Check if image is null
-        if(image == null) {
-            // Print Error message
-            java.lang.System.out.println("Error: cannot draw null image.\n");
-            return;
-        }
-        // Draw image on screen at (x,y) with size (w,h)
-        mGraphics.drawImage(image, (int)x, (int)y, (int)w, (int)h, null);
-    }
-
-
-}
 
 
 
@@ -283,11 +184,7 @@ class TimeSystem extends Timer {
                 double passedTime = measureTime();
                 double dt = passedTime / 1000.;
 
-                // Update the Game
-                //update(dt);
 
-                // Tell the Game to draw
-                //mPanel.repaint();
             }
         });
         return timer;
@@ -295,12 +192,6 @@ class TimeSystem extends Timer {
 }
 
 
-//Update Image
-//?Update velocity of player
-class UpdateSystem extends System{
-
-
-}
 
 //Needs implementation
 class AudioManagement extends System{
@@ -417,48 +308,6 @@ class KeyEventSystem extends System {
 
 
 
-
-    // public KeyEventSystem(ArrayList<Entity> entities, KeyEvent event){
-    //     //keyReleased-Called whenever a key is released
-    //     // Left Arrow
-    //     if(event.getKeyCode() == KeyEvent.VK_LEFT) {
-    //         // Record Left Movement
-    //         //left = false;
-    //     }
-    //     // Right Arrow
-    //     if(event.getKeyCode() == KeyEvent.VK_RIGHT) {
-    //         // Record Right Movement
-    //         //right = false;
-    //     }
-
-
-
-    //     // Register a key event dispatcher to get a turn in handling all
-    //     // key events, independent of which component currently has the focus
-    //     KeyboardFocusManager.getCurrentKeyboardFocusManager()
-    //             .addKeyEventDispatcher(new KeyEventDispatcher() {
-    //                 @Override
-    //                 public boolean dispatchKeyEvent(KeyEvent e) {
-    //                     switch (e.getID()) {
-    //                         case KeyEvent.KEY_PRESSED:
-    //                             GameEngine.this.keyPressed(e);
-    //                             return false;
-    //                         case KeyEvent.KEY_RELEASED:
-    //                             GameEngine.this.keyReleased(e);
-    //                             return false;
-    //                         case KeyEvent.KEY_TYPED:
-    //                             GameEngine.this.keyTyped(e);
-    //                             return false;
-    //                         default:
-    //                             return false; // do not consume the event
-    //                     }
-    //                 }
-    //             });
-
-
-
-
-    // }
 }
 
 //Counting points/scores
@@ -551,20 +400,13 @@ class GameSystem extends System{
         gameSystem.InitiateGame();
 
 
-        //gameSystem.CreateEntities();
 
-        //Loading Pictures to Window
-        //RenderSystem renderSystem = new RenderSystem();
-        //renderSystem.Process(entities,mGraphics);
-        //renderSystem.Process(entities, (Graphics2D)graphics);
 
         ActionListener listener = null;
 
         TimeSystem timeSystem = new TimeSystem(framerate,listener);
 
-        // Start the Game
-        //Testing Loop?duplicate loop with Game_Panel?
-        //gameSystem.GameLoop(timeSystem.GetTimer(),framerate);
+
     }
 
     //Initiate Default Stats apart from Entities
@@ -577,42 +419,6 @@ class GameSystem extends System{
 
     }
 
-//
-//    public static void TestingDrawPic(ArrayList<Entity> entities,Graphics2D g){
-//
-//        PositionComponent playerPosition=(PositionComponent)entities.get(0).getComponent(PositionComponent.class);
-//        RenderComponent playerRender=(RenderComponent) entities.get(0).getComponent(RenderComponent.class);
-//        playerRender.getImage();
-//        // Save current transform
-//        AffineTransform transform = g.getTransform();
-//
-//        // Move into correct position
-//        g.translate(playerPosition.getX(), playerPosition.getY());
-//
-//        // Draw the Render component
-//        g.drawImage(playerRender.getImage(), 0, 0, null);
-//
-//        // Reset Transform
-//        g.setTransform(transform);
-//
-//    }
-
-//    //Entities Stats default - ERROR NOT GAME_PANEL NOT DRAWING THIS OUT
-//    public void CreateEntities(){
-//        //Add PlayerEntity - Index 0
-//        AddPlayerEntity();
-//
-//        //Create platforms - Index 1
-//        AddPlatformEntity();
-//
-//        //Create Coins - Index 2
-//        AddCoinEntity();
-//
-//        //Create Background - Index 3
-//        AddBackgroundEntity();
-//
-//
-//    }
     public void AddPlayerEntity(){
         //Needs PlayerComponent,PositionComponent,VelocityComponent,RenderComponent
         Entity Player = new Entity();
@@ -628,35 +434,6 @@ class GameSystem extends System{
 
 
     }
-//        public void AddPlatformEntity(){
-//
-//        Entity Platform1 = new Entity();
-//        Entity Platform2 = new Entity();
-//        Entity Platform3 = new Entity();
-//        Entity Platform4 = new Entity();
-//        Entity Platform5 = new Entity();
-//        Entity Platform6 = new Entity();
-//        Platform1.addComponent(new PlatformComponent(100, 500, 100, 20));
-//        Platform2.addComponent(new PlatformComponent(350, 350, 80, 20));
-//        Platform3.addComponent(new PlatformComponent(600, 300, 60, 20));
-//        Platform4.addComponent(new PlatformComponent(850, 350, 40, 20));
-//        Platform5.addComponent(new PlatformComponent(1000, 300, 20, 20));
-//        Platform6.addComponent(new PlatformComponent(1200, 200, 10, 20));
-//        //Below is equal to Platforms.addComponent(new RenderComponent(SelectedImage));
-//        RenderSystem.LoadPicturesToEntity(Platform1,"Pictures/platform/platform.png");
-//        RenderSystem.LoadPicturesToEntity(Platform2,"Pictures/platform/platform.png");
-//        RenderSystem.LoadPicturesToEntity(Platform3,"Pictures/platform/platform.png");
-//        RenderSystem.LoadPicturesToEntity(Platform4,"Pictures/platform/platform.png");
-//        RenderSystem.LoadPicturesToEntity(Platform5,"Pictures/platform/platform.png");
-//        RenderSystem.LoadPicturesToEntity(Platform6,"Pictures/platform/platform.png");
-//        entities.add(Platform1);
-//        entities.add(Platform2);
-//        entities.add(Platform3);
-//        entities.add(Platform4);
-//        entities.add(Platform5);
-//        entities.add(Platform6);
-//
-//    }
      public void AddPlatformEntity(){
 
          Entity Platform1 = new Entity();
@@ -775,50 +552,6 @@ class GameSystem extends System{
 
 
 
-    //Constructor - TO IMPLEMENT
-    public void GameSystem(Entity entity){
-        if(entity.hasComponent(RenderComponent.class)&&entity.hasComponent(TransformComponent.class)){
-            RenderComponent renderComponent = (RenderComponent) entity.getComponent(RenderComponent.class);
-            TransformComponent transformComponent = (TransformComponent)entity.getComponent((TransformComponent.class));
-
-
-
-            this.mTransforms = transformComponent.GetTransform();
-            mGraphics = renderComponent.Graphics();
-            // Create graphics transform stack
-            mTransforms = new Stack<AffineTransform>();
-
-
-        }
-
-
-
-        // This gets called any time the Operating System
-        // tells the program to paint itself
-        // Paint the game
-        RenderSystem renderSystem = new RenderSystem();
-
-        if (initialised) {
-            GameSystem.this.paintComponent(renderSystem.mGraphics);
-        }
-
-
-
-    }
-
-
-
-
-    public void GameLoop(TimeSystem timer, int framerate){
-        initialised = true; // assume init has been called or won't be called
-
-        timer.setFramerate(framerate);
-        timer.setRepeats(true);
-
-        // Main loop runs until program is closed
-        timer.start();
-
-    }
 }
 
 
@@ -925,39 +658,7 @@ class MovementSystem extends System {
 
                 RenderSystem.UpdatePicturesToEntity(entity,"Pictures/player/"+playerStatus+String.valueOf(playerMovement)+".png",playerMoveLeft);
 
-                
-                   //java.lang.System.out.println("jumping");
-                   //new Thread(new thread()).start();
-                    //VelocityComponent.setY(-100);
-                    //VelocityComponent.setY(-100);
-                    //VelocityComponent.setY(-100);
-                    //java.lang.System.out.println("Player'x: " + Entity);
 
-                    /* try
-                    {
-                       // VelocityComponent.setY(-100);
-                        Thread.sleep(1000);
-                    }
-                    catch(InterruptedException ex)
-                    {
-                        Thread.currentThread().interrupt();
-                    }*/
-                   //  VelocityComponent.setY(+100);
-                    //VelocityComponent.setY(+100);
-
-
-
-
-
-
-
-                // if(position.getX() > 250 && velocity.Get_VelocityX() > 0) {
-                //     velocity.setX(velocity.Get_VelocityX() * -1);
-                // }
-
-                // if(position.getX() < 50 && velocity.Get_VelocityX() < 0) {
-                //     velocity.setX(velocity.Get_VelocityX() * -1);
-                // }
             }
         }
     }
@@ -1059,19 +760,6 @@ class RenderSystem extends System {
 
         }
     }
-    int mWidth, mHeight;
-    Graphics2D mGraphics;
-    boolean initialised = false;
 
 
-    //Constructor
-    public void RenderSystem() {
-
-
-    }
-
-}
-
-class CollideSystem extends System{
-    Area E_boundingbox,E_Area;
 }
